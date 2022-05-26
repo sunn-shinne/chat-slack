273cc168470462826a38cbfea03a79ef6c621128
Ekaterina <brekhova.katya@gmail.com>
2022-05-26 12:33:27 +0300

    Create 404 page

--- src/containers/NotFound.jsx
+++ src/containers/NotFound.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 
 const NotFound = () => {
   return (
-    <div className="card h-100">
-      <div className="card-body">
-        <p className="card-title">Not Found</p>
-      </div>
+    <div className="m-3 text-center">
+      <img
+        src="https://i7.uihere.com/icons/280/662/360/404-01-a905605f38e8c7b16da0fed6e0f1c638.png"
+        alt="Страница не найдена"
+        style={{ height: 400, }}
+      />
+      <h1 className="h4 text-muted">Страница не найдена</h1>
+      <p>
+        Вы можете перейти&nbsp;
+        <a href="/">на главную страницу</a>
+      </p>
     </div>
   );
 }
