9d6affe9efd96c85072d2c117233c58f23137760
Ekaterina <brekhova.katya@gmail.com>
2022-05-27 12:40:35 +0300

    Fix linter problems

--- src/containers/NotFound.jsx
+++ src/containers/NotFound.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
 
-const NotFound = () => {
-  return (
-    <div className="m-3 text-center">
-      <img
-        src="https://i7.uihere.com/icons/280/662/360/404-01-a905605f38e8c7b16da0fed6e0f1c638.png"
-        alt="Страница не найдена"
-        style={{ height: 400, }}
-      />
-      <h1 className="h4 text-muted">Страница не найдена</h1>
-      <p>
-        Вы можете перейти&nbsp;
-        <a href="/">на главную страницу</a>
-      </p>
-    </div>
-  );
-}
+const NotFound = () => (
+  <div className="m-3 text-center">
+    <img
+      src="https://i7.uihere.com/icons/280/662/360/404-01-a905605f38e8c7b16da0fed6e0f1c638.png"
+      alt="Страница не найдена"
+      style={{ height: 400 }}
+    />
+    <h1 className="h4 text-muted">Страница не найдена</h1>
+    <p>
+      Вы можете перейти&nbsp;
+      <a href="/">на главную страницу</a>
+    </p>
+  </div>
+);
 
 export default NotFound;
