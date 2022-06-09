f967bab216b42626cf54beefbe9b2e431397a872
Ekaterina <brekhova.katya@gmail.com>
2022-06-09 13:20:56 +0300

    Add filtering of obscene words

--- src/components/Chat/ChatMessage.jsx
+++ src/components/Chat/ChatMessage.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import filter from 'leo-profanity';
+
+filter.add(filter.getDictionary('ru'));
 
 const ChatMessage = ({ senderName, text }) => (
   <div className="text-break mb-2">
     <b>{senderName}</b>
-    {`: ${text}`}
+    {`: ${filter.clean(text)}`}
   </div>
 );
 
