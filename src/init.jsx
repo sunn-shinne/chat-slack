3a56af41e0a96593387f27812fcd39ce65429cd2
Ekaterina <brekhova.katya@gmail.com>
2022-06-09 14:27:33 +0300

    Intergate Rollbar

--- src/init.jsx
+++ src/init.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Provider } from 'react-redux';
+import { Provider as StoreProvider } from 'react-redux';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
+import Rollbar from 'rollbar';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
 import App from './components/App.jsx';
@@ -8,16 +10,31 @@ import ChatApiProvider from './contexts/ChatApiProvider.js';
 import AuthApiProvider from './contexts/AuthApiProvider.js';
 import ModalApiProvider from './contexts/ModalApiProvider.js';
 
+const rollbarConfig = {
+  accessToken: `${process.env.ROLLBAR_ACCESS_TOKEN}`,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  payload: {
+    environment: 'production',
+  },
+};
+
+const rollbar = new Rollbar(rollbarConfig);
+
 export default (socket) => (
-  <Provider store={store}>
-    <I18nextProvider i18n={i18n}>
-      <AuthApiProvider>
-        <ModalApiProvider>
-          <ChatApiProvider socket={socket}>
-            <App />
-          </ChatApiProvider>
-        </ModalApiProvider>
-      </AuthApiProvider>
-    </I18nextProvider>
-  </Provider>
+  <RollbarProvider config={rollbarConfig} instance={rollbar}>
+    <ErrorBoundary>
+      <StoreProvider store={store}>
+        <I18nextProvider i18n={i18n}>
+          <AuthApiProvider>
+            <ModalApiProvider>
+              <ChatApiProvider socket={socket}>
+                <App />
+              </ChatApiProvider>
+            </ModalApiProvider>
+          </AuthApiProvider>
+        </I18nextProvider>
+      </StoreProvider>
+    </ErrorBoundary>
+  </RollbarProvider>
 );
